6c43c47df5f2c54c2f3be3136361b861de99a0e9
Kirill Koval <brcyrill2@gmail.com>
2022-12-15 13:55:15 +0200

    added money_account model

---
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/model/MoneyAccount.java
@@ -0,0 +1,12 @@
+package com.kerrrusha.lab234.model;
+
+import java.time.LocalDateTime;
+
+public class MoneyAccount {
+
+    private int id;
+    private String name;
+    private int owner_user_id;
+    private LocalDateTime created_time;
+    private LocalDateTime updated_time;
+}
