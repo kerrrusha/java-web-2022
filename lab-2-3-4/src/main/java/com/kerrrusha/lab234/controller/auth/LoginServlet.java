5e931817f222491c47004a83add7271d51b97ee5
Kirill Koval <brcyrill2@gmail.com>
2022-12-16 00:43:34 +0200

    added LoginServlet

---
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/controller/auth/LoginServlet.java
@@ -0,0 +1,22 @@
+package com.kerrrusha.lab234.controller.auth;
+
+import com.kerrrusha.lab234.service.auth.AuthResultSender;
+import com.kerrrusha.lab234.service.auth.LoginService;
+import com.kerrrusha.lab234.service.auth.result.AuthResult;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/auth/login")
+public class LoginServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		final String phone = request.getParameter("phone");
+		final String password = request.getParameter("password");
+
+		AuthResult result = new LoginService(phone, password).processLogin(request);
+		AuthResultSender.valueOf(result).sendResponse(response);
+	}
+}
