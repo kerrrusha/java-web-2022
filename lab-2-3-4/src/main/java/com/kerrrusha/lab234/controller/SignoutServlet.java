b6742139c306fa044727374ab89cf4e1b36cc3e5
Kirill Koval <brcyrill2@gmail.com>
2022-12-15 16:35:12 +0200

    changed servlet library source

--- lab-2-3-4/src/main/java/com/kerrrusha/lab234/controller/SignoutServlet.java
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/controller/SignoutServlet.java
@@ -1,17 +1,19 @@
 package com.kerrrusha.lab234.controller;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+@WebServlet(name = "SignoutServlet", urlPatterns = "/signout")
 public class SignoutServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        //do signout ...
         request.getRequestDispatcher("index.jsp").forward(request, response);
     }
 }
