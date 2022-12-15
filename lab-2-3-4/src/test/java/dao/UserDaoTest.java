f0bc81c7dc2b985e5b15da2ab717eb21d8191488
Kirill Koval <brcyrill2@gmail.com>
2022-12-15 14:43:30 +0200

    added UserDaoTest

---
+++ lab-2-3-4/src/test/java/dao/UserDaoTest.java
@@ -0,0 +1,55 @@
+package dao;
+
+import com.kerrrusha.lab234.dao.DBException;
+import com.kerrrusha.lab234.dao.user.UserDao;
+import com.kerrrusha.lab234.factory.UserFactory;
+import com.kerrrusha.lab234.model.User;
+import org.apache.log4j.Logger;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserDaoTest {
+
+    private static final Logger logger = Logger.getLogger(UserDaoTest.class);
+
+    private final UserDao userDao;
+
+    public UserDaoTest() throws DBException {
+        userDao = new UserDao();
+    }
+
+    @Test
+    public void findAllTest() throws DBException {
+        Collection<User> allUsers = userDao.findAll();
+        logger.info("Collected " + allUsers.size() + " users.");
+    }
+
+    @Test
+    public void findOneByIdTest() throws DBException {
+        User user = userDao.findOneById(1);
+        assertNotNull(user);
+    }
+
+    @Test
+    public void findOneByPhoneTest() throws DBException {
+        User user = userDao.findOneByPhone("+380123456789");
+        assertNotNull(user);
+    }
+
+    @Test
+    public void insertDeleteTest() throws DBException {
+        User newUser = UserFactory.createExample();
+
+        userDao.insert(newUser);
+
+        User sameUserFromDb = userDao.findOneByPhone(newUser.getPhone());
+        assertNotNull(sameUserFromDb);
+        assertEquals(newUser, sameUserFromDb);
+
+        userDao.deleteByPhone(sameUserFromDb.getPhone());
+        assertNull(userDao.findOneByPhone(sameUserFromDb.getPhone()));
+    }
+}
