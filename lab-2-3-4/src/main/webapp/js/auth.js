2da95687763904bc178eb02beda6280575511854
Kirill Koval <brcyrill2@gmail.com>
2022-12-15 15:34:42 +0200

    added auth.js

---
+++ lab-2-3-4/src/main/webapp/js/auth.js
@@ -0,0 +1,139 @@
+function processLogin(url) {
+	hideErrorsBlock();
+
+	let login = $(".login-show input[placeholder=Nickname]").val();
+	let password = $(".login-show input[placeholder=Password]").val();
+
+	let errors = [];
+	if (!validateLogin(login)) {
+		errors.push("Login must be at least 3 characters long and must not contain spaces.");
+	}
+	if (!validatePassword(password)) {
+		errors.push("Password must be at least 3 characters long and must not contain spaces.");
+	}
+
+	if (errors.length !== 0) {
+		showErrors(errors);
+		return;
+	}
+
+	let data = "login="+login+"&password="+password;
+	$.ajax({
+	  type: "POST",
+	  url: url,
+	  data: data,
+	  success: function(response) {
+		  console.log(response);
+		  if (response["status"] === 200) {
+			  location.reload();
+		  } else {
+			  showErrors(response["errorPool"]);
+		  }
+	  },
+	  error: function (response) {
+	      console.log(response);
+	  }
+	});
+}
+
+function hideErrorsBlock() {
+	$(".errors-block").hide();
+}
+
+function showErrors(errors) {
+	let errorsBlock = $(".errors-block")
+	errorsBlock.css('visibility', 'visible');
+	errorsBlock.show();
+	errorsBlock.html(createErrorsHtmlList(errors));
+}
+
+function createErrorsHtmlList(errors) {
+	let html = "<ul class='list-group'>";
+	errors.forEach(error => {
+		html += "<li class='list-group-item list-group-item-danger'>" + error + "</li>";
+	});
+	html += "</ul>";
+	return html;
+}
+
+function validateLogin(login) {
+	return login.trim().length >= 3 && !login.includes(" ");
+}
+
+function isCharNumber(c) {
+  return c >= '0' && c <= '9';
+}
+
+function validatePhone(phone) {
+	phone = phone.trim()
+	if (phone.length !== 13) {
+		return false;
+	}
+	if (phone.charAt(0) !== '+') {
+		return false;
+	}
+	for (let i = 1; i < phone.length; i++) {
+		if (!isCharNumber(phone.charAt(i))) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function validateName(name) {
+	return name.trim().length >= 3 && !name.includes(" ");
+}
+
+function validatePassword(password) {
+	return password.length >= 3 && !password.includes(" ");
+}
+
+function processRegister(registerUrl, afterRegisterUrl) {
+	hideErrorsBlock();
+
+	let firstName = $("#firstName").val();
+	let lastName = $("#lastName").val();
+	let phone = $("#phone").val();
+	let password = $("#password").val();
+	let passwordRepeat = $("#passwordRepeat").val();
+
+	let errors = [];
+	if (!validateName(firstName)) {
+		errors.push("First name must be 3 symbols at least.");
+	}
+	if (!validateName(lastName)) {
+		errors.push("Last name must be 3 symbols at least.");
+	}
+	if (!validatePhone(phone)) {
+		errors.push("Phone must be in format like '+380123456789'.");
+	}
+	if (!validatePassword(password) || !validatePassword(passwordRepeat)) {
+		errors.push("Password must be at least 3 characters long and must not contain spaces.");
+	}
+	if (password !== passwordRepeat) {
+		errors.push("Passwords don't match.");
+	}
+
+	if (errors.length !== 0) {
+		showErrors(errors);
+		return;
+	}
+
+	let data = "firstName="+firstName+"&lastName="+lastName+"&phone="+phone+"&password="+password+"&passwordRepeat="+passwordRepeat;
+	$.ajax({
+		type: "POST",
+		url: registerUrl,
+		data: data,
+		success: function(response) {
+			console.log(response);
+			if (response["status"] === 200) {
+				window.location.replace(afterRegisterUrl);
+			} else {
+				showErrors(response["errorPool"]);
+			}
+		},
+		error: function (response) {
+			console.log(response);
+		}
+	});
+}
