621b00e9fd543fd63b132c38ae483d8d013dc0c1
Kirill Koval <brcyrill2@gmail.com>
2022-12-15 14:18:23 +0200

    added getters/setters

--- lab-2-3-4/src/main/java/com/kerrrusha/lab234/model/User.java
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/model/User.java
@@ -12,4 +12,60 @@ public class User {
     private int role_id;
     private LocalDateTime created_time;
     private LocalDateTime updated_time;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setLastName(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRoleId(int role_id) {
+        this.role_id = role_id;
+    }
+
+    public void setCreatedTime(LocalDateTime created_time) {
+        this.created_time = created_time;
+    }
+
+    public void setUpdatedTime(LocalDateTime updated_time) {
+        this.updated_time = updated_time;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return first_name;
+    }
+
+    public String getLastName() {
+        return last_name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getRoleId() {
+        return role_id;
+    }
 }
