902b5b6a1377f2b08023423f7e2fe230c2301bb6
Kirill Koval <brcyrill2@gmail.com>
2022-12-15 16:35:01 +0200

    changed servlet library source

--- lab-2-3-4/src/main/java/com/kerrrusha/lab234/controller/IndexServlet.java
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/controller/IndexServlet.java
@@ -2,9 +2,13 @@ package com.kerrrusha.lab234.controller;
 
 import java.io.*;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+@WebServlet(name = "IndexServlet", urlPatterns = {"/index", ""})
 public class IndexServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
