a5c241644c4f018be1b9bac9d16ae6f3bde23ddf
Kirill Koval <brcyrill2@gmail.com>
2022-12-15 14:18:41 +0200

    added user Fields

---
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/dao/user/constant/Fields.java
@@ -0,0 +1,14 @@
+package com.kerrrusha.lab234.dao.user.constant;
+
+public class Fields {
+
+	public static final String USER_ID = "id";
+	public static final String USER_FIRST_NAME = "first_name";
+	public static final String USER_LAST_NAME = "last_name";
+	public static final String USER_PHONE = "phone";
+	public static final String USER_PASSWORD = "password";
+	public static final String USER_ROLE_ID = "role_id";
+	public static final String USER_CREATED_TIME = "created_time";
+	public static final String USER_UPDATED_TIME = "updated_time";
+
+}
