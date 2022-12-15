9e4b742b5e4d1002fda33b89546ddfb5b4ec2412
Kirill Koval <brcyrill2@gmail.com>
2022-12-15 13:54:38 +0200

    added user model

---
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/model/User.java
@@ -0,0 +1,15 @@
+package com.kerrrusha.lab234.model;
+
+import java.time.LocalDateTime;
+
+public class User {
+
+    private int id;
+    private String first_name;
+    private String last_name;
+    private String phone;
+    private String password;
+    private int role_id;
+    private LocalDateTime created_time;
+    private LocalDateTime updated_time;
+}
