752acd6a157aaac9a2a6e7563f6e1c00e36a02f5
Kirill Koval <brcyrill2@gmail.com>
2022-12-16 01:30:18 +0200

    added AboutServlet

---
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/controller/AboutServlet.java
@@ -0,0 +1,15 @@
+package com.kerrrusha.lab234.controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet("/about")
+public class AboutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("about.jsp").forward(request, response);
+    }
+}
