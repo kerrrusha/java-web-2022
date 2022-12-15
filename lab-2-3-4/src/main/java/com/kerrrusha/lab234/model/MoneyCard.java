dc6e0905f0557ab3267500cf4325cfe9c70c395c
Kirill Koval <brcyrill2@gmail.com>
2022-12-15 13:55:32 +0200

    added money_card model

---
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/model/MoneyCard.java
@@ -0,0 +1,12 @@
+package com.kerrrusha.lab234.model;
+
+import java.time.LocalDateTime;
+
+public class MoneyCard {
+
+    private int id;
+    private int money_account_id;
+    private int balance;
+    private LocalDateTime created_time;
+    private LocalDateTime updated_time;
+}
