89121131179a385e685f879e495c94c8188d4d24
Kirill Koval <brcyrill2@gmail.com>
2022-12-15 13:55:24 +0200

    added blocked_money_account model

---
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/model/BlockedMoneyAccount.java
@@ -0,0 +1,11 @@
+package com.kerrrusha.lab234.model;
+
+import java.time.LocalDateTime;
+
+public class BlockedMoneyAccount {
+
+    private int id;
+    private int blocked_money_account_id;
+    private int blocked_by_user_id;
+    private LocalDateTime created_time;
+}
