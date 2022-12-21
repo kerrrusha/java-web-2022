d72a99a7b2bada2c042a072f8c91736c8a7ba02f
Kirill Koval <brcyrill2@gmail.com>
2022-12-21 23:37:44 +0200

    added new features

--- lab-2-3-4/src/main/java/com/kerrrusha/lab234/model/MoneyAccount.java
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/model/MoneyAccount.java
@@ -9,4 +9,44 @@ public class MoneyAccount {
     private int owner_user_id;
     private LocalDateTime created_time;
     private LocalDateTime updated_time;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setOwnerUserId(int owner_user_id) {
+        this.owner_user_id = owner_user_id;
+    }
+
+    public void setCreatedTime(LocalDateTime created_time) {
+        this.created_time = created_time;
+    }
+
+    public void setUpdatedTime(LocalDateTime updated_time) {
+        this.updated_time = updated_time;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getOwnerUserId() {
+        return owner_user_id;
+    }
+
+    public LocalDateTime getCreatedTime() {
+        return created_time;
+    }
+
+    public LocalDateTime getUpdatedTime() {
+        return updated_time;
+    }
 }
