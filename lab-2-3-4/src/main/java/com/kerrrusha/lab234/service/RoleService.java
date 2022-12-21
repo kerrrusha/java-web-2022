21f969a1c64488f1df43bc01b734bded85d07b30
Kirill Koval <brcyrill2@gmail.com>
2022-12-21 21:18:50 +0200

    code refactor

--- lab-2-3-4/src/main/java/com/kerrrusha/lab234/service/RoleService.java
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/service/RoleService.java
@@ -2,8 +2,11 @@ package com.kerrrusha.lab234.service;
 
 import com.kerrrusha.lab234.dao.DBException;
 import com.kerrrusha.lab234.dao.role.RoleDao;
+import com.kerrrusha.lab234.model.User;
 
 public class RoleService {
+
+    private static final String ADMIN_NAME = "admin";
 
     private final RoleDao roleDao;
 
@@ -13,5 +16,9 @@ public class RoleService {
 
     public String getRoleNameById(int roleId) throws DBException {
         return roleDao.findOneById(roleId).getName();
+    }
+
+    public boolean userIsAdmin(User user) throws DBException {
+        return roleDao.findOneById(user.getRoleId()).getName().equalsIgnoreCase(ADMIN_NAME);
     }
 }
