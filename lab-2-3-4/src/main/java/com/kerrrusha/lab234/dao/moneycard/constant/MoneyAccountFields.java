356f626b08252195ed41c99604de92fdde47c050
Kirill Koval <brcyrill2@gmail.com>
2022-12-21 23:35:35 +0200

    added MoneyAccountFields

---
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/dao/moneycard/constant/MoneyAccountFields.java
@@ -0,0 +1,11 @@
+package com.kerrrusha.lab234.dao.moneycard.constant;
+
+public class MoneyAccountFields {
+
+    public static final String MONEY_ACCOUNT_ID = "id";
+    public static final String MONEY_ACCOUNT_NAME = "name";
+    public static final String MONEY_ACCOUNT_OWNER_USER_ID = "owner_user_id";
+    public static final String MONEY_ACCOUNT_CREATED_TIME = "created_time";
+    public static final String MONEY_ACCOUNT_UPDATED_TIME = "updated_time";
+
+}
