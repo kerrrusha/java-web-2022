5bd0dda19af7e6e96c9e0631d2409868bde5c574
Kirill Koval <brcyrill2@gmail.com>
2022-12-21 23:36:42 +0200

    added new queries

--- lab-2-3-4/src/main/java/com/kerrrusha/lab234/dao/user/constant/Queries.java
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/dao/user/constant/Queries.java
@@ -4,7 +4,8 @@ public class Queries {
 
 	public static final String SELECT_ALL_USERS = "SELECT * FROM `user`";
 
-	public static final String INSERT_USER = "INSERT INTO `user`(first_name, last_name, phone, password, role_id) VALUES (?, ?, ?, ?, ?)";
+	public static final String INSERT_USER = "INSERT INTO `user`(first_name, last_name, phone, password, role_id)" +
+			" VALUES (?, ?, ?, ?, ?)";
 
 	public static final String FIND_USER_BY_ID = "SELECT * FROM `user` WHERE id = ?";
 	public static final String FIND_USER_BY_PHONE = "SELECT * FROM `user` WHERE phone = ?";
