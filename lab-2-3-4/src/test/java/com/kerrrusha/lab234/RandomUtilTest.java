af1cc60a0295fcb81f58e1bcfab857b36cdfbf07
Kirill Koval <brcyrill2@gmail.com>
2022-12-21 23:34:41 +0200

    added random util test

---
+++ lab-2-3-4/src/test/java/com/kerrrusha/lab234/RandomUtilTest.java
@@ -0,0 +1,19 @@
+package com.kerrrusha.lab234;
+
+import org.junit.jupiter.api.Test;
+
+import static com.kerrrusha.lab234.util.RandomUtil.generateRandomNDigits;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RandomUtilTest {
+
+    @Test
+    public void generateRandomNDigitsTest() {
+        final int TESTS_AMOUNT = 100;
+        final int DIGITS_AMOUNT = 3;
+
+        for (int i = 0; i < TESTS_AMOUNT; i++) {
+            assertEquals(DIGITS_AMOUNT, generateRandomNDigits(DIGITS_AMOUNT).length());
+        }
+    }
+}
