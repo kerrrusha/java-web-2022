51c6b2eb3d6c27302903c17fcbd43c362c512ca8
Kirill Koval <brcyrill2@gmail.com>
2022-12-21 21:19:02 +0200

    added setters

--- lab-2-3-4/src/main/java/com/kerrrusha/lab234/model/MoneyCard.java
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/model/MoneyCard.java
@@ -1,5 +1,6 @@
 package com.kerrrusha.lab234.model;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class MoneyCard {
@@ -7,6 +8,41 @@ public class MoneyCard {
     private int id;
     private int money_account_id;
     private int balance;
+    private String number;
+    private String secret;
+    private LocalDate expiration_date;
     private LocalDateTime created_time;
     private LocalDateTime updated_time;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setMoneyAccountId(int money_account_id) {
+        this.money_account_id = money_account_id;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    public void setExpirationDate(LocalDate expiration_date) {
+        this.expiration_date = expiration_date;
+    }
+
+    public void setCreatedTime(LocalDateTime created_time) {
+        this.created_time = created_time;
+    }
+
+    public void setUpdatedTime(LocalDateTime updated_time) {
+        this.updated_time = updated_time;
+    }
 }
