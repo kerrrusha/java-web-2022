0ab7d1acdaadf7e8f675a669fcb267534c098833
Kirill Koval <brcyrill2@gmail.com>
2022-12-21 23:37:53 +0200

    added new features

--- lab-2-3-4/src/main/java/com/kerrrusha/lab234/service/auth/AuthResultSender.java
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/service/auth/AuthResultSender.java
@@ -1,14 +1,9 @@
 package com.kerrrusha.lab234.service.auth;
 
 import com.google.gson.Gson;
-import com.kerrrusha.lab234.dao.DBException;
-import com.kerrrusha.lab234.service.auth.result.AuthResult;
-import org.apache.http.HttpStatus;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Collections;
 
 import static com.kerrrusha.lab234.util.ServletUtil.setJsonToResponse;
 
