5e9739a3dcd4e12ff11d2940ac9b7d1b1b91fef8
Kirill Koval <brcyrill2@gmail.com>
2022-12-22 02:57:36 +0200

    added new features

--- lab-2-3-4/src/main/webapp/js/moneycards.js
+++ lab-2-3-4/src/main/webapp/js/moneycards.js
@@ -1,3 +1,32 @@
-function loadData(moneycards) {
-    console.log(moneycards);
+function loadData() {
+    $.ajax({
+        type: "POST",
+        url: window.location.href,
+        success: function(response) {
+            console.log(response);
+            fillTable(response);
+        },
+        error: function (response) {
+            console.log(response);
+        }
+    });
+}
+
+function fillTable(elements) {
+    let tableBody = $("#moneyCardsTableBody");
+    tableBody.html(mapElementsToHtml(elements));
+}
+
+function mapElementsToHtml(elements) {
+    let html = '';
+    elements.forEach(element => {
+        html += '<tr>' +
+            '        <th scope="row">'+element.moneyAccountId+'</th>' +
+            '    <td>'+element.moneyAccountName+'</td>' +
+            '    <td>'+element.moneyCardNumber+'</td>' +
+            '    <td>'+element.balance+'</td>' +
+            '    <td>'+element.expirationDate+'</td>' +
+            '</tr>';
+    });
+    return html;
 }
