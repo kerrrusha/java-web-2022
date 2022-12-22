89a3ab218e49cf3cde4901c030a37c64764fdf5c
Kirill Koval <brcyrill2@gmail.com>
2022-12-22 02:56:38 +0200

    added new queries

--- lab-2-3-4/src/main/java/com/kerrrusha/lab234/dao/moneycard/constant/Queries.java
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/dao/moneycard/constant/Queries.java
@@ -3,11 +3,13 @@ package com.kerrrusha.lab234.dao.moneycard.constant;
 public class Queries {
 
 	public static final String FIND_MONEY_CARDS_BY_USER_ID = "SELECT * FROM `money_card` " +
-			"WHERE money_account_id = (select id from `money_account` where owner_user_id = ?)";
+			"WHERE money_account_id in (select id from `money_account` where owner_user_id = ?)";
 	public static final String FIND_MONEY_ACCOUNT_BY_USER_ID_AND_NAME = "SELECT * FROM `money_account` " +
-			"WHERE id = (select id from `user` where user_id = ?) and name = ? order by id desc";
+			"WHERE owner_user_id = (select id from `user` where id = ?) and name = ? order by id desc";
 	public static final String FIND_MONEY_CARD_BY_USER_ID_AND_SECRET_AND_NUMBER = "SELECT * FROM `money_card` " +
-			"WHERE id = (select id from `user` where user_id = ?) and secret = ? and number = ?";
+			"WHERE money_account_id in (select id FROM `money_account` " +
+			"WHERE owner_user_id = (select id from `user` where id = ?)) and secret = ? and number = ?";
+	public static final String FIND_MONEY_ACCOUNT_BY_ID = "SELECT * FROM `money_account` WHERE id = ?";
 
 	public static final String INSERT_MONEY_CARD = "INSERT INTO " +
 			"`money_card`(money_account_id, number, expiration_date, secret, balance) " +
