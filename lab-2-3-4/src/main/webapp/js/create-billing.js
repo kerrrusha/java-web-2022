f68d3ab87ebe0a468a0498a8918b57e6ec7316ca
Kirill Koval <brcyrill2@gmail.com>
2022-12-24 04:01:29 +0200

    added CreateBilling js

---
+++ lab-2-3-4/src/main/webapp/js/create-billing.js
@@ -0,0 +1,98 @@
+function sendCreateNewBillingRequest(postUrl) {
+    let cardNumber = $("#recipient-card-number").val();
+    let moneyAccountId = new URLSearchParams(window.location.search).get("fromMoneyAccountId");
+    let data = "fromMoneyAccountId="+moneyAccountId+"&toMoneyCardNumber="+cardNumber;
+    $.ajax({
+        type: "POST",
+        url: postUrl,
+        data: data,
+        success: function(response) {
+            console.log(response);
+            if (response["status"] === 200) {
+                showSuccess(response["senderMoneyCard"]);
+            } else {
+                showErrors(response["errorPool"]);
+            }
+        },
+        error: function (response) {
+            console.log(response);
+            showErrors(response["Something went wrong. Please try again later."]);
+        }
+    });
+}
+
+function updateButtonStatus() {
+    hideResultBlock();
+
+    let cardNumber = $("#recipient-card-number").val();
+    let button = document.getElementById("postButton");
+
+    let errors = [];
+    if (cardNumberHasInvalidLength(cardNumber)) {
+        errors.push("Card number must be 16 symbols length.");
+    }
+    if (cardNumberHasNonNumberSymbols(cardNumber)) {
+        errors.push("Card number cannot contain non-digit symbols.");
+    }
+    showErrors(errors);
+    button.disabled = errors.length > 0;
+}
+
+function cardNumberHasInvalidLength(number) {
+    return number.length !== 16;
+}
+
+function cardNumberHasNonNumberSymbols(number) {
+    const regex = new RegExp('[^0-9]');
+    return regex.test(number);
+}
+
+function hideResultBlock() {
+    $("#result").hide();
+}
+
+function showSuccess(moneyCard) {
+    let resultBlock = $("#result");
+    resultBlock.css('visibility', 'visible');
+    resultBlock.show();
+    resultBlock.html(createSuccessHtml(moneyCard));
+}
+
+function createSuccessHtml(moneyCard) {
+    return '<div class="alert alert-success" role="alert">' +
+        '  Billing was sent successfully! Your balance: ' + getPrettyMoney(moneyCard.balance) +
+        '</div>';
+}
+
+function getPrettyMoney(totalCents) {
+    let dollars = removeDigitsAfterComma(totalCents / 100);
+    let cents = totalCents - dollars * 100;
+    return dollars + "." + toCentsFormat(cents) + " $";
+}
+
+function toCentsFormat(int) {
+    if (int < 10) {
+        return "0" + int;
+    }
+    return "" + int;
+}
+
+function removeDigitsAfterComma(double) {
+    return double | 0;
+}
+
+function showErrors(errors) {
+    let resultBlock = $("#result")
+    resultBlock.css('visibility', 'visible');
+    resultBlock.show();
+    resultBlock.html(createErrorsHtmlList(errors));
+}
+
+function createErrorsHtmlList(errors) {
+    let html = "<ul class='list-group'>";
+    errors.forEach(error => {
+        html += "<li class='list-group-item list-group-item-danger'>" + error + "</li>";
+    });
+    html += "</ul>";
+    return html;
+}
