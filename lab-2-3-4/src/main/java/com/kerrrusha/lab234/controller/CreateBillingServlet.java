89ae199500396abced0387d6f34a16173def082c
Kirill Koval <brcyrill2@gmail.com>
2022-12-24 04:01:17 +0200

    added CreateBillingServlet

---
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/controller/CreateBillingServlet.java
@@ -0,0 +1,47 @@
+package com.kerrrusha.lab234.controller;
+
+import com.kerrrusha.lab234.dao.DBException;
+import com.kerrrusha.lab234.exception.RestrictedMoneyAccountException;
+import com.kerrrusha.lab234.model.User;
+import com.kerrrusha.lab234.service.moneycard.MoneyCardService;
+import com.kerrrusha.lab234.validator.BillingValidator;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
+
+@WebServlet("/create-billing")
+public class CreateBillingServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        final String fromMoneyAccountIdStr = request.getParameter("fromMoneyAccountId");
+        final User user = (User)request.getSession().getAttribute("user");
+
+        try {
+            int fromMoneyAccountId = Integer.parseInt(fromMoneyAccountIdStr);
+            Collection<String> errorPool = new BillingValidator(user, fromMoneyAccountId).getErrors();
+            if (!errorPool.isEmpty()) {
+                throw new RestrictedMoneyAccountException(errorPool);
+            }
+            MoneyCardService moneyCardService = new MoneyCardService(user);
+
+            request.setAttribute("moneyAccount", moneyCardService.getMoneyAccountById(fromMoneyAccountId));
+            request.setAttribute("moneyCard", moneyCardService.getMoneyCardByMoneyAccountId(fromMoneyAccountId));
+        } catch (NumberFormatException | DBException | RestrictedMoneyAccountException e) {
+            request.setAttribute("error", e.getMessage());
+            request.getRequestDispatcher("error.jsp").forward(request, response);
+            return;
+        }
+
+        request.getRequestDispatcher("create-billing.jsp").forward(request, response);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        final String fromMoneyAccountId = request.getParameter("fromMoneyAccountId");
+        final String toMoneyCardNumber = request.getParameter("toMoneyCardNumber");
+    }
+}
