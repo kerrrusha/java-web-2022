9dc6be2c1dc74f2adc1208269b0d44734311eec7
Kirill Koval <brcyrill2@gmail.com>
2022-12-25 03:01:47 +0200

    added BillingResultSender

---
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/service/moneycard/result/billing/BillingResultSender.java
@@ -0,0 +1,29 @@
+package com.kerrrusha.lab234.service.moneycard.result.billing;
+
+import com.google.gson.Gson;
+import com.kerrrusha.lab234.factory.GsonFactory;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static com.kerrrusha.lab234.util.ServletUtil.setJsonToResponse;
+
+public class BillingResultSender extends BillingResult {
+
+	private final Gson gson = GsonFactory.create();
+
+	public static BillingResultSender valueOf(BillingResult anotherResult) {
+		BillingResultSender sender = new BillingResultSender();
+
+		sender.setStatus(anotherResult.getStatus());
+		sender.setErrorPool(anotherResult.getErrorPool());
+		sender.setMoneyCard(anotherResult.getMoneyCard());
+
+		return sender;
+	}
+
+	public void sendResponse(HttpServletResponse response) throws IOException {
+		setJsonToResponse(response, gson.toJson(this));
+		response.getWriter().flush();
+	}
+}
