73943a158a3f0e4f2e3487ecb1d2e2402d65f439
Kirill Koval <brcyrill2@gmail.com>
2022-12-25 03:00:57 +0200

    added new method

--- lab-2-3-4/src/main/java/com/kerrrusha/lab234/util/ValidatorUtil.java
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/util/ValidatorUtil.java
@@ -7,4 +7,8 @@ public class ValidatorUtil {
 	public static Optional<String> checkIfFieldIsNull(Object field, final String errorMessage) {
 		return field == null ? Optional.of(errorMessage) : Optional.empty();
 	}
+
+	public static Optional<String> checkIfStringIsBlank(String str, final String errorMessage) {
+		return str == null || str.isEmpty() ? Optional.of(errorMessage) : Optional.empty();
+	}
 }
