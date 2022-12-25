4c20e38bf5a5ea0edd9ddf635dfe07a23b420fef
Kirill Koval <brcyrill2@gmail.com>
2022-12-25 15:52:37 +0200

    refactor, new features

--- lab-2-3-4/src/main/java/com/kerrrusha/lab234/dao/billing/BillingDao.java
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/dao/billing/BillingDao.java
@@ -2,11 +2,15 @@ package com.kerrrusha.lab234.dao.billing;
 
 import com.kerrrusha.lab234.dao.AbstractDao;
 import com.kerrrusha.lab234.dao.DBException;
+import com.kerrrusha.lab234.dao.billing.constant.BillingStatusFields;
 import com.kerrrusha.lab234.dao.billing.constant.Fields;
 import com.kerrrusha.lab234.dao.billing.constant.Queries;
 import com.kerrrusha.lab234.model.Billing;
+import com.kerrrusha.lab234.model.BillingStatus;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static com.kerrrusha.lab234.util.SqlUtil.mapToLocalDateTime;
 
@@ -26,6 +30,49 @@ public class BillingDao extends AbstractDao {
 			e.printStackTrace();
 			throw new DBException(e.getMessage());
 		}
+	}
+
+	public Collection<Billing> findBillingsByUserId(int userId) throws DBException {
+		Collection<Billing> entities = new ArrayList<>();
+		try (Connection con = DriverManager.getConnection(FULL_URL);
+			 PreparedStatement stmt = con.prepareStatement(Queries.FIND_BILLINS_BY_USER_ID)) {
+			stmt.setInt(1, userId);
+			try (ResultSet rs = stmt.executeQuery()) {
+				while (rs.next()) {
+					entities.add(mapToEntity(rs));
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DBException(e.getMessage());
+		}
+		return entities;
+	}
+
+	public BillingStatus findBillingStatusById(int billingStatusId) throws DBException {
+		BillingStatus entity = null;
+		try (Connection con = DriverManager.getConnection(FULL_URL);
+			 PreparedStatement stmt = con.prepareStatement(Queries.FIND_BILLING_STATUS_BY_ID)) {
+			stmt.setInt(1, billingStatusId);
+			try (ResultSet rs = stmt.executeQuery()) {
+				if (rs.next()) {
+					entity = mapToBillingStatus(rs);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DBException(e.getMessage());
+		}
+		return entity;
+	}
+
+	private BillingStatus mapToBillingStatus(ResultSet rs) throws SQLException {
+		BillingStatus billingStatus = new BillingStatus();
+
+		billingStatus.setId(rs.getInt(BillingStatusFields.BILLING_STATUS_ID));
+		billingStatus.setName(rs.getString(BillingStatusFields.BILLING_STATUS_NAME));
+
+		return billingStatus;
 	}
 
 	private Billing mapToEntity(ResultSet rs) throws SQLException {
