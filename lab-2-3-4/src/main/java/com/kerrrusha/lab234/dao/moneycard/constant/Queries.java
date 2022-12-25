e1f4d326fbc2726fd310caf46ee4824ded49820a
Kirill Koval <brcyrill2@gmail.com>
2022-12-25 02:58:50 +0200

    added new query

--- lab-2-3-4/src/main/java/com/kerrrusha/lab234/dao/moneycard/constant/Queries.java
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/dao/moneycard/constant/Queries.java
@@ -11,6 +11,7 @@ public class Queries {
 			"WHERE owner_user_id = (select id from `user` where id = ?)) and secret = ? and number = ?";
 	public static final String FIND_MONEY_ACCOUNT_BY_ID = "SELECT * FROM `money_account` WHERE id = ?";
 	public static final String FIND_MONEY_CARD_BY_MONEY_ACCOUNT_ID = "SELECT * FROM `money_card` WHERE money_account_id = ?";
+	public static final String FIND_MONEY_CARDS_BY_NUMBER = "SELECT * FROM `money_card` WHERE number = ?";
 
 	public static final String INSERT_MONEY_CARD = "INSERT INTO " +
 			"`money_card`(money_account_id, number, expiration_date, secret, balance) " +
@@ -20,6 +21,9 @@ public class Queries {
 			"values (?, ?)";
 
 	public static final String UPDATE_MONEY_CARD_NUMBER = "UPDATE `money_card` " +
-			"set number = ?" +
+			"set number = ? " +
+			"where id = ?";
+	public static final String UPDATE_MONEY_CARD_BALANCE = "UPDATE `money_card` " +
+			"set balance = ? " +
 			"where id = ?";
 }
