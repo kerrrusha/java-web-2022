eb62be26b2cbfcf188fe9752b2c2ac3067041e85
Kirill Koval <brcyrill2@gmail.com>
2022-12-25 02:58:35 +0200

    refactor

--- lab-2-3-4/src/main/java/com/kerrrusha/lab234/service/auth/AuthResult.java
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/service/auth/AuthResult.java
@@ -1,30 +1,11 @@
 package com.kerrrusha.lab234.service.auth;
 
 import com.kerrrusha.lab234.model.User;
+import com.kerrrusha.lab234.service.AbstractResult;
 
-import java.util.Collection;
+public class AuthResult extends AbstractResult {
 
-public class AuthResult {
-
-	private Collection<String> errorPool;
-	private int status;
 	private transient User user;
-
-	public Collection<String> getErrorPool() {
-		return errorPool;
-	}
-
-	public void setErrorPool(Collection<String> errorPool) {
-		this.errorPool = errorPool;
-	}
-
-	public int getStatus() {
-		return status;
-	}
-
-	public void setStatus(int status) {
-		this.status = status;
-	}
 
 	public User getUser() {
 		return user;
