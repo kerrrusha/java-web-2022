de086915f296089fccd9f642ffc52506343fdec3
Kirill Koval <brcyrill2@gmail.com>
2022-12-25 03:01:41 +0200

    added BillingResult

---
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/service/moneycard/result/billing/BillingResult.java
@@ -0,0 +1,17 @@
+package com.kerrrusha.lab234.service.moneycard.result.billing;
+
+import com.kerrrusha.lab234.model.MoneyCard;
+import com.kerrrusha.lab234.service.AbstractResult;
+
+public class BillingResult extends AbstractResult {
+
+    private MoneyCard moneyCard;
+
+    public MoneyCard getMoneyCard() {
+        return moneyCard;
+    }
+
+    public void setMoneyCard(MoneyCard moneyCard) {
+        this.moneyCard = moneyCard;
+    }
+}
