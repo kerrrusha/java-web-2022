d01c636e6919e6bc8a45635f4dbdd78bd5b5feac
Kirill Koval <brcyrill2@gmail.com>
2022-12-25 15:51:43 +0200

    added BillingStatusFields

---
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/dao/billing/constant/BillingStatusFields.java
@@ -0,0 +1,8 @@
+package com.kerrrusha.lab234.dao.billing.constant;
+
+public class BillingStatusFields {
+
+    public static final String BILLING_STATUS_ID = "id";
+    public static final String BILLING_STATUS_NAME = "name";
+
+}
