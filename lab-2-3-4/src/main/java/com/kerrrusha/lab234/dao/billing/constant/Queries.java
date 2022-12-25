92fd9360d0a331e3b6fc14f8d48087e877cef4d2
Kirill Koval <brcyrill2@gmail.com>
2022-12-25 15:53:12 +0200

    refactor, new features

--- lab-2-3-4/src/main/java/com/kerrrusha/lab234/dao/billing/constant/Queries.java
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/dao/billing/constant/Queries.java
@@ -5,4 +5,9 @@ public class Queries {
 	public static final String INSERT_BILLING = "INSERT INTO " +
 			"`billing`(money_amount, from_money_card_id, to_money_card_id, billing_status_id) " +
 			"values (?, ?, ?, ?)";
+
+    public static final String FIND_BILLINS_BY_USER_ID = "SELECT * FROM `billing` " +
+			"WHERE from_money_card_id in (select id from `money_card` where money_account_id in " +
+			"(select id from `money_account` where owner_user_id = ?))";
+    public static final String FIND_BILLING_STATUS_BY_ID = "select * from `billing_status` where id = ?";
 }
