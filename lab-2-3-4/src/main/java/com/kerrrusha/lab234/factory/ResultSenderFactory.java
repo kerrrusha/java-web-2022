f51593dad9c16d0b31d2c07fcafb06e3879c337d
Kirill Koval <brcyrill2@gmail.com>
2022-12-25 03:02:04 +0200

    added ResultSenderFactory

---
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/factory/ResultSenderFactory.java
@@ -0,0 +1,16 @@
+package com.kerrrusha.lab234.factory;
+
+import com.kerrrusha.lab234.service.AbstractResult;
+import com.kerrrusha.lab234.service.moneycard.AbstractResultSender;
+
+import static java.util.Collections.singletonList;
+
+public class ResultSenderFactory {
+
+    public static AbstractResultSender createAbstractResultSenderFromError(String error, int status) {
+        AbstractResult result = new AbstractResult();
+        result.setErrorPool(singletonList(error));
+        result.setStatus(status);
+        return AbstractResultSender.valueOf(result);
+    }
+}
