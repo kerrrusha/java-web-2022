02c4f9013fda38dcd6b6a42cb06555fef152f1d6
Kirill Koval <brcyrill2@gmail.com>
2022-12-25 15:52:41 +0200

    refactor, new features

--- lab-2-3-4/src/main/java/com/kerrrusha/lab234/model/BillingStatus.java
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/model/BillingStatus.java
@@ -4,4 +4,20 @@ public class BillingStatus {
 
     private int id;
     private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
