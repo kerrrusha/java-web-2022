038b196372910fa9be3cf9fc339d8838dc805915
Kirill Koval <brcyrill2@gmail.com>
2022-12-25 14:46:24 +0200

    added BillingDao Fields

---
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/dao/billing/constant/Fields.java
@@ -0,0 +1,12 @@
+package com.kerrrusha.lab234.dao.billing.constant;
+
+public class Fields {
+
+	public static final String BILLING_ID = "id";
+	public static final String BILLING_MONEY_AMOUNT = "money_amount";
+	public static final String BILLING_FROM_MONEY_CARD_ID = "from_money_card_id";
+	public static final String BILLING_TO_MONEY_CARD_ID = "to_money_card_id";
+	public static final String BILLING_STATUS_ID = "billing_status_id";
+	public static final String BILLING_CREATED_TIME = "created_time";
+
+}
