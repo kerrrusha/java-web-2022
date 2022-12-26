6060d381110afe4163d8d0fbd06252f98495ad3e
Kirill Koval <brcyrill2@gmail.com>
2022-12-26 21:24:42 +0200

    refactor

--- lab-2-3-4/src/main/java/com/kerrrusha/lab234/model/BlockedMoneyAccount.java
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/model/BlockedMoneyAccount.java
@@ -8,4 +8,20 @@ public class BlockedMoneyAccount {
     private int blocked_money_account_id;
     private int blocked_by_user_id;
     private LocalDateTime created_time;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setBlockedMoneyAccountId(int blocked_money_account_id) {
+        this.blocked_money_account_id = blocked_money_account_id;
+    }
+
+    public void setBlockedByUserId(int blocked_by_user_id) {
+        this.blocked_by_user_id = blocked_by_user_id;
+    }
+
+    public void setCreatedTime(LocalDateTime created_time) {
+        this.created_time = created_time;
+    }
 }
