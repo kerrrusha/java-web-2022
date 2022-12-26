ac6b4eb11d91d8d6b2bf65c39e3bc29b476bb576
Kirill Koval <brcyrill2@gmail.com>
2022-12-26 21:22:50 +0200

    refactor

--- lab-2-3-4/src/main/java/com/kerrrusha/lab234/exception/RestrictedMoneyAccountException.java
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/exception/RestrictedMoneyAccountException.java
@@ -2,11 +2,9 @@ package com.kerrrusha.lab234.exception;
 
 import java.util.Collection;
 
-import static java.util.stream.Collectors.joining;
-
-public class RestrictedMoneyAccountException extends Exception {
+public class RestrictedMoneyAccountException extends ErrorPoolException {
 
     public RestrictedMoneyAccountException(Collection<String> errorPool) {
-        super(errorPool.stream().collect(joining(System.lineSeparator())));
+        super(errorPool);
     }
 }
