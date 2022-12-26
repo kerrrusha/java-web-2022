ad13c76386397d9ccd7557de5e97b2642e306938
Kirill Koval <brcyrill2@gmail.com>
2022-12-26 21:24:07 +0200

    rename

--- lab-2-3-4/src/main/java/com/kerrrusha/lab234/validator/MoneyCardCreationValidator.java
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/validator/MoneyCardCreationValidator.java
@@ -1,42 +1,41 @@
 package com.kerrrusha.lab234.validator;
 
-import java.util.Collection;
 import java.util.Optional;
 import java.util.regex.Pattern;
 
 import static com.kerrrusha.lab234.util.ValidatorUtil.checkIfFieldIsNull;
 
-public class MoneyCardValidator extends AbstractValidator {
+public class MoneyCardCreationValidator extends AbstractValidator {
 
     private static final String INVALID_NAME_LENGTH = "Card name must be at least 3 symbols length.";
     private static final String NAME_CONTAINS_SPACES_ERROR = "Name cannot contain spaces.";
     private static final String NAME_CONTAINS_NON_LETTER_SYMBOLS = "Card name should consist only of english letters.";
     private static final String NAME_IS_NULL = "Something is wrong while submitting money card name to the server. We will definitely fix this, but for now, please try again.";
 
-    private final String name;
+    private final String moneyAccountName;
 
-    public MoneyCardValidator(String name) {
-        this.name = name;
+    public MoneyCardCreationValidator(String moneyAccountName) {
+        this.moneyAccountName = moneyAccountName;
     }
 
     @Override
     protected void validate() {
-        addPossibleError(checkIfFieldIsNull(name, NAME_IS_NULL));
+        addPossibleError(checkIfFieldIsNull(moneyAccountName, NAME_IS_NULL));
         addPossibleError(validateLength());
         addPossibleError(validateSpaces());
         addPossibleError(validateNonLetterSymbols());
     }
 
     private Optional<String> validateLength() {
-        return name.length() < 3 ? Optional.of(INVALID_NAME_LENGTH) : Optional.empty();
+        return moneyAccountName.length() < 3 ? Optional.of(INVALID_NAME_LENGTH) : Optional.empty();
     }
 
     private Optional<String> validateSpaces() {
-        return name.contains(" ") ? Optional.of(NAME_CONTAINS_SPACES_ERROR) : Optional.empty();
+        return moneyAccountName.contains(" ") ? Optional.of(NAME_CONTAINS_SPACES_ERROR) : Optional.empty();
     }
 
     private Optional<String> validateNonLetterSymbols() {
-        return Pattern.compile("[^a-zA-Z ]").matcher(name).find()
+        return Pattern.compile("[^a-zA-Z ]").matcher(moneyAccountName).find()
                 ? Optional.of(NAME_CONTAINS_NON_LETTER_SYMBOLS)
                 : Optional.empty();
     }
