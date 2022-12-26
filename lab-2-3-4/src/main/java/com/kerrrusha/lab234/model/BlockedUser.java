d3d484d3943ca36df0090dd3d01192522591fc00
Kirill Koval <brcyrill2@gmail.com>
2022-12-26 21:38:20 +0200

    added setters

--- lab-2-3-4/src/main/java/com/kerrrusha/lab234/model/BlockedUser.java
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/model/BlockedUser.java
@@ -8,4 +8,20 @@ public class BlockedUser {
     private int blocked_user_id;
     private int blocked_by_user_id;
     private LocalDateTime created_time;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setBlockedUserId(int blocked_user_id) {
+        this.blocked_user_id = blocked_user_id;
+    }
+
+    public void setBlockedByUserId(int blocked_by_user_id) {
+        this.blocked_by_user_id = blocked_by_user_id;
+    }
+
+    public void setCreatedTime(LocalDateTime created_time) {
+        this.created_time = created_time;
+    }
 }
