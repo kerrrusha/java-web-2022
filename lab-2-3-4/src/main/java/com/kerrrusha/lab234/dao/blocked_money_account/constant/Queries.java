ec7b90089e029d06ab089b858df993156dfb1176
Kirill Koval <brcyrill2@gmail.com>
2022-12-27 01:50:24 +0200

    added new queries

--- lab-2-3-4/src/main/java/com/kerrrusha/lab234/dao/blocked_money_account/constant/Queries.java
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/dao/blocked_money_account/constant/Queries.java
@@ -6,4 +6,6 @@ public class Queries {
 			"where blocked_money_account_id = ?";
     public static final String INSERT_BLOCKED_MONEY_ACCOUNT = "INSERT INTO `blocked_money_account`(blocked_money_account_id, blocked_by_user_id)" +
             " VALUES (?, ?)";
+    public static final String DELETE_BLOCKED_MONEY_ACCOUNT = "delete from `blocked_money_account` " +
+            "where blocked_money_account_id = ?";
 }
