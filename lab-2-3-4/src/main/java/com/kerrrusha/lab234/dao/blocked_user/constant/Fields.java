d65379fce220866cbfa990b6101596d34f0017b9
Kirill Koval <brcyrill2@gmail.com>
2022-12-26 21:37:46 +0200

    added BlockedUserDao Fields

---
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/dao/blocked_user/constant/Fields.java
@@ -0,0 +1,9 @@
+package com.kerrrusha.lab234.dao.blocked_user.constant;
+
+public class Fields {
+
+    public static final String BLOCKED_USER_ID = "id";
+	public static final String BLOCKED_USER_USER_ID = "blocked_user_id";
+	public static final String BLOCKED_USER_BLOCKED_BY_USER_ID = "blocked_by_user_id";
+	public static final String BLOCKED_USER_CREATED_TIME = "created_time";
+}
