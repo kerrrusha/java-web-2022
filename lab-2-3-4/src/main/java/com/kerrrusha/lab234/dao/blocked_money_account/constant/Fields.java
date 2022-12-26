2f773c2abe98cf001280a36916a2cb47f354fc57
Kirill Koval <brcyrill2@gmail.com>
2022-12-26 21:21:25 +0200

    added BlockedMoneyAccount fields

---
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/dao/blocked_money_account/constant/Fields.java
@@ -0,0 +1,9 @@
+package com.kerrrusha.lab234.dao.blocked_money_account.constant;
+
+public class Fields {
+
+    public static final String BLOCKED_MONEY_ACCOUNT_ID = "id";
+	public static final String BLOCKED_MONEY_ACCOUNT_BLOCKED_MONEY_ACCOUNT_ID = "blocked_money_account_id";
+	public static final String BLOCKED_MONEY_ACCOUNT_BLOCKED_BY_USER_ID = "blocked_by_user_id";
+	public static final String BLOCKED_MONEY_ACCOUNT_CREATED_TIME = "created_time";
+}
