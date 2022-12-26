3db5c6f5db1aa41c9b24589d4899bc9ce87e182c
Kirill Koval <brcyrill2@gmail.com>
2022-12-26 21:38:00 +0200

    refactor

--- lab-2-3-4/src/main/java/com/kerrrusha/lab234/dao/blocked_money_account/constant/Queries.java
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/dao/blocked_money_account/constant/Queries.java
@@ -2,7 +2,7 @@ package com.kerrrusha.lab234.dao.blocked_money_account.constant;
 
 public class Queries {
 
-    public static final String FIND_BY_BLOCKED_MONEY_ACCOUNT = "select * from `blocked_money_account` " +
+    public static final String FIND_BY_BLOCKED_MONEY_ACCOUNT_ID = "select * from `blocked_money_account` " +
 			"where blocked_money_account_id = ?";
     public static final String INSERT_BLOCKED_MONEY_ACCOUNT = "INSERT INTO `blocked_money_account`(blocked_money_account_id, blocked_by_user_id)" +
             " VALUES (?, ?)";
