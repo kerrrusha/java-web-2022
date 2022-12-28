d2ad9934d86cdad9b68708ec9151942c5adcb377
Kirill Koval <brcyrill2@gmail.com>
2022-12-28 21:18:40 +0200

    added new methods

--- lab-2-3-4/src/main/java/com/kerrrusha/lab234/viewmodel/MoneycardViewModel.java
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/viewmodel/MoneycardViewModel.java
@@ -1,6 +1,7 @@
 package com.kerrrusha.lab234.viewmodel;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class MoneycardViewModel {
 
@@ -43,5 +44,18 @@ public class MoneycardViewModel {
 
     public void setOpenedDate(LocalDate openedDate) {
         this.openedDate = openedDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MoneycardViewModel viewModel = (MoneycardViewModel) o;
+        return moneyAccountId == viewModel.moneyAccountId && isBlocked == viewModel.isBlocked && balance == viewModel.balance && Objects.equals(moneyAccountName, viewModel.moneyAccountName) && Objects.equals(moneyCardNumber, viewModel.moneyCardNumber) && Objects.equals(secret, viewModel.secret) && Objects.equals(expirationDate, viewModel.expirationDate) && Objects.equals(openedDate, viewModel.openedDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(moneyAccountId, isBlocked, moneyAccountName, moneyCardNumber, balance, secret, expirationDate, openedDate);
     }
 }
