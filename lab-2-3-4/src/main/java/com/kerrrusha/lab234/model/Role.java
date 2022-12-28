c3415d9f67989b581e6fccbf24016e63ea1b8428
Kirill Koval <brcyrill2@gmail.com>
2022-12-28 21:18:44 +0200

    added new methods

--- lab-2-3-4/src/main/java/com/kerrrusha/lab234/model/Role.java
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/model/Role.java
@@ -1,5 +1,7 @@
 package com.kerrrusha.lab234.model;
 
+import java.util.Objects;
+
 public class Role {
 
     private int id;
@@ -19,5 +21,26 @@ public class Role {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return getId() == role.getId() && Objects.equals(getName(), role.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
