a58208091ad9d3e595f4e0961bb67edbf61d8844
Kirill Koval <brcyrill2@gmail.com>
2022-12-28 21:19:18 +0200

    added new test

---
+++ lab-2-3-4/src/test/java/com/kerrrusha/lab234/dao/AbstractDaoTest.java
@@ -0,0 +1,11 @@
+package com.kerrrusha.lab234.dao;
+
+import org.junit.jupiter.api.Test;
+
+class AbstractDaoTest {
+
+    @Test
+    public void connectionSuccessfullTest() throws DBException {
+        new AbstractDao();
+    }
+}
