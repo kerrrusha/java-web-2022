0b012a17b9c1ba31e5871c8c9788487ffa183020
Kirill Koval <brcyrill2@gmail.com>
2022-12-28 21:18:09 +0200

    added new methods

--- lab-2-3-4/src/main/java/com/kerrrusha/lab234/model/BlockedMoneyAccount.java
+++ lab-2-3-4/src/main/java/com/kerrrusha/lab234/model/BlockedMoneyAccount.java
@@ -1,6 +1,7 @@
 package com.kerrrusha.lab234.model;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class BlockedMoneyAccount {
 
@@ -17,11 +18,32 @@ public class BlockedMoneyAccount {
         this.blocked_money_account_id = blocked_money_account_id;
     }
 
+    public int getBlockedMoneyAccountId() {
+        return blocked_money_account_id;
+    }
+
+    public int getBlockedByUserId() {
+        return blocked_by_user_id;
+    }
+
     public void setBlockedByUserId(int blocked_by_user_id) {
         this.blocked_by_user_id = blocked_by_user_id;
     }
 
     public void setCreatedTime(LocalDateTime created_time) {
         this.created_time = created_time;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BlockedMoneyAccount that = (BlockedMoneyAccount) o;
+        return id == that.id && blocked_money_account_id == that.blocked_money_account_id && blocked_by_user_id == that.blocked_by_user_id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, blocked_money_account_id, blocked_by_user_id);
     }
 }
